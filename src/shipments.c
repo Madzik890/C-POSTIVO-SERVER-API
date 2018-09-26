7341ebefcfc061c1b8469e394abced0c35daae48
madzik890 <Madzik890@hotmail.com>
2018-09-26 23:47:58 +0200

    Add save shipments to file

--- src/shipments.c
+++ src/shipments.c
@@ -1,5 +1,12 @@
 #include "shipments.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+void saveShipmentToFile(struct ns1__Shipment * shipment);
+
+/// <summary>
+/// Converts recipient object to shipment struct.
+/// </summary>
 void convertRecipientToShipment(struct ArrayOfRecipients *recipients, struct ArrayOfShipments **shipments)
 {
   for(int i = 0; i < recipients->__size; i++)
@@ -20,5 +27,43 @@ void convertRecipientToShipment(struct ArrayOfRecipients *recipients, struct Arr
     (*(*shipments)->__ptr[i]->page_USCOREnumber) = 2;
     (*shipments)->__ptr[i]->price = malloc(sizeof(float));
     (*(*shipments)->__ptr[i]->price) = 2.56f;
+
+    saveShipmentToFile((*shipments)->__ptr[i]);
+  }
+}
+
+/// <summary>
+/// Saves shipment object to file.
+/// </summary>
+/// <param name = "shipment"> Pointer to shipment object </param>
+void saveShipmentToFile(struct ns1__Shipment * shipment)
+{
+  FILE * m_file;
+  char * s_fileDir;
+  //strcpy(s_fileDir, "dispatches/");       // copies "one" into str_output
+  strcat(s_fileDir, shipment->recipient_USCOREname);
+  
+  m_file = fopen(s_fileDir, "ab+");
+  if(m_file != NULL)
+  {
+    fwrite(shipment->recipient_USCOREname, 1, strlen(shipment->recipient_USCOREname), m_file);
+    fwrite("\n", 1, 1, m_file);
+
+    fwrite(shipment->recipient_USCOREcity, 1, strlen(shipment->recipient_USCOREcity), m_file);
+    fwrite("\n", 1, 1, m_file);
+
+    fwrite(shipment->recipient_USCOREpost_USCOREcode, 1, strlen(shipment->recipient_USCOREpost_USCOREcode), m_file);
+    fwrite("\n", 1, 1, m_file);
+
+    fwrite(shipment->recipient_USCOREaddress, 1, strlen(shipment->recipient_USCOREaddress), m_file);
+    fwrite("\n", 1, 1, m_file);
+
+    fwrite(shipment->recipient_USCOREhome_USCOREnumber, 1, strlen(shipment->recipient_USCOREhome_USCOREnumber), m_file);
+    fwrite("\n", 1, 1, m_file);
+
+    fwrite(shipment->recipient_USCOREflat_USCOREnumber, 1, strlen(shipment->recipient_USCOREflat_USCOREnumber), m_file);
+    fwrite("\n", 1, 1, m_file);
+
+    fclose(m_file);
   }
 }
