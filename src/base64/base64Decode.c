666996a8570d20a3302b5f3e31617228e0b84a99
madzik890 <Madzik890@hotmail.com>
2018-10-05 21:19:55 +0200

    Back to old decode method

--- src/base64/base64Decode.c
+++ src/base64/base64Decode.c
@@ -1,6 +1,5 @@
 //Decodes Base64
 #include "base64Decode.h"
-//Decodes Base64
 #include <stdio.h>
 #include <string.h>
 #include <openssl/bio.h>
@@ -8,35 +7,21 @@
 #include <stdint.h>
 #include <assert.h>
 
-size_t calcDecodeLength(const char* b64input) 
-{ //Calculates the length of a decoded string
-	size_t len = strlen(b64input),
-		padding = 0;
+char *b64_decode(unsigned char *input, int length)
+{
+  char *res = malloc(length + 1); 
+  BIO *bmem = BIO_new_mem_buf(input, length);
+  BIO *b64 = BIO_new(BIO_f_base64());
+  BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
+  bmem = BIO_push(b64, bmem);
 
-	if (b64input[len-1] == '=' && b64input[len-2] == '=') //last two chars are =
-		padding = 2;
-	else if (b64input[len-1] == '=') //last char is =
-		padding = 1;
+  long long n = BIO_read(bmem, res, length);
+  if (n > 0)
+    res[n] = 0;
+  else
+   res[0] = 0; // note: this is an error state.
 
-	return (len*3)/4 - padding;
-}
+  BIO_free_all(bmem);
 
-int base64Decode(char* b64message, unsigned char** buffer, size_t* length) 
-{ //Decodes a base64 encoded string
-	BIO *bio, *b64;
-
-	int decodeLen = calcDecodeLength(b64message);
-	*buffer = (unsigned char*)malloc(decodeLen + 1);
-	(*buffer)[decodeLen] = '\0';
-
-	bio = BIO_new_mem_buf(b64message, -1);
-	b64 = BIO_new(BIO_f_base64());
-	bio = BIO_push(b64, bio);
-
-	BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL); //Do not use newlines to flush buffer
-	*length = BIO_read(bio, *buffer, strlen(b64message));
-	assert(*length == decodeLen); //length should equal decodeLen, else something went horribly wrong
-	BIO_free_all(bio);
-
-	return (0); //success
+  return res;
 }
--- src/base64/base64Decode.h
+++ src/base64/base64Decode.h
@@ -1,11 +1,6 @@
-/*
- *  https://gist.github.com/barrysteyn/7308212
- */
 #ifndef BASE64_H
 #define BASE64_H
 
-#include <stdio.h>
-
-int base64Decode(char* b64message, unsigned char** buffer, size_t* length); //Decodes a base64 encoded string
+char *b64_decode(unsigned char *input, int length);
 
 #endif
